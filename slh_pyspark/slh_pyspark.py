1da3f8f1e51728352d85ea9e24d66ca9701e6f85
Sl4sh3r99 <138933611+Sl4sh3r999@users.noreply.github.com>
2023-11-01 12:41:51 -0600

    Update slh_pyspark.py, now with examples

--- slh_pyspark/slh_pyspark.py
+++ slh_pyspark/slh_pyspark.py
@@ -1,32 +1,51 @@
 def col_a_list(df, col):
 
-    df_inter = df.select(f'{col}')
-    df_inter = df_inter.dropDuplicates()
-    n = df_inter.columns[0]
-    row = df_inter.collect()
-    s = str(row)
-    s = s.replace('[', '').replace(']', '').replace(f'Row({n}=', '').replace(')', '').replace("'", "")
-    ls = s.split(', ')
+    try:
+        df_inter = df.select(f'{col}')
+        df_inter = df_inter.dropDuplicates()
+        n = df_inter.columns[0]
+        row = df_inter.collect()
+        s = str(row)
+        s = s.replace('[', '').replace(']', '').replace(f'Row({n}=', '').replace(')', '').replace("'", "")
+        ls = s.split(', ')
+    except:
+        print(f'Example:\nslh.col_a_list(Dataframe, "Colum_Name")')
+        ls = None
 
     return ls
 
 def col_a_tupla(df, col):
 
-    df_inter = df.select(f'{col}')
-    df_inter = df_inter.dropDuplicates()
-    n = df_inter.columns[0]
-    row = df_inter.collect()
-    s = str(row)
-    s = s.replace('[', '').replace(']', '').replace(f'Row({n}=', '').replace(')', '').replace("'", "")
-    tp = tuple(s.split(', '))
+    try:
+        df_inter = df.select(f'{col}')
+        df_inter = df_inter.dropDuplicates()
+        n = df_inter.columns[0]
+        row = df_inter.collect()
+        s = str(row)
+        s = s.replace('[', '').replace(']', '').replace(f'Row({n}=', '').replace(')', '').replace("'", "")
+        tp = tuple(s.split(', '))
+    except:
+        print(f'Example:\nslh.col_a_tupla(Dataframe, "Colum_Name")')
+        tp = None
 
     return tp
 
-def cols_para_sql_athena(schema_str):
-    import re
+def cols_para_sql_athena(esquema):
 
-    schema_str = schema_str.replace('-', '').replace('|', '').replace(':', '').replace(' (nullable = true)', ',')
-    pattern = re.compile(r'(\w+)\s+string')
-    result = re.sub(pattern, r'"\1" string', schema_str)
+    print(f'Example:\nslh.cols_para_sql_athena(df.printSchema)\nImportant: do not use "()" at the end of DF funtion\n\n')
 
-    return result
+    estring = str(esquema)
+    
+    estring = estring.split('DataFrame[')[-1].split(']')[0]
+    lineas = estring.split(', ')
+    lineas = [linea.strip() for linea in lineas if linea.strip()]
+    
+    separacion = ',\n'.join(lineas)
+    separacion = separacion.replace(':', '')
+    
+    try:
+        separacion = separacion.replace('long', 'bigint')
+    except:
+        pass
+
+    return separacion
